4bea0bd776abc88ebe19abb3109e4ad1222a62b2
Douglas Pardim <douglas.pardim@globalsys.com.br>
2024-08-30 23:36:40 -0300

    fix: asset prefix problems

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "/",
+  assetPrefix: "/pokeshop",
   basePath: "/pokeshop",
   reactStrictMode: true,
   images: {
