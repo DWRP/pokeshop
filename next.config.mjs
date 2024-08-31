a51fd99145f522bcda07949335da8ed1f890ed0d
Douglas Pardim <douglas.pardim@globalsys.com.br>
2024-08-30 23:21:19 -0300

    fix: change next config again

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "/pokeshop/",
+  assetPrefix: "/",
+  basePath: "/pokeshop/",
   reactStrictMode: true,
   images: {
     dangerouslyAllowSVG: true,
